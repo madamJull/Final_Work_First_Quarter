f797580ec1841a8d1c54527a91f21a399677fb57
Juliya <9123288799@mail/ru>
2023-01-05 20:04:49 +0500

    Add complete task

--- TASK/Program.cs
+++ TASK/Program.cs
@@ -1,3 +1,34 @@
-﻿// Написать программу, которая их имеющегося массива строк формирует массив из строк, дина которых меньше либо равна 3 символа. 
+﻿// Написать программу, которая их имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+
+string[] array1 = {"world", "-3", "add", "good", "monday"};
+string[] array2 = new string[array1.Length];
+
+NewMassiv(array1, array2);
+PrintArray(array1);
+Console.Write("-> "); 
+PrintArray(array2);
+
+void NewMassiv(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if (array1[i].Length < 4)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} "); 
+    }
+    Console.WriteLine();
+}
